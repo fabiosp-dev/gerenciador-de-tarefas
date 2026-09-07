2ccadcbff0b18b3c49a302cc1666f4166633041b
Fábio <fabio.pinheiro1187@gmail.com>
2026-09-07 15:24:28 -0300

    feat: adiciona gerenciamento de tarefas

--- main.py
+++ main.py
@@ -17,19 +17,50 @@ while opcao != "0":
 
 
     if opcao == "1":
-        print("Você escolheu Adicionar Tarefa")
-        tarefa = input("digite sua tarefa: ")
-        tarefas.append(tarefa)
+        tarefa = input("Digite sua tarefa: ")
+
+        nova_tarefa = {
+            "descricao": tarefa,
+            "concluida": False
+        }
+
+        tarefas.append(nova_tarefa)
 
 
     elif opcao == "2":
         print("Você escolheu Listar Tarefas")
         cont = 1
         for tarefa in tarefas:
-            print(f"{cont} - {tarefa}")
+            if tarefa["concluida"]:
+                status = "[x]"
+            else:
+                status = "[ ]"
+            print(f"{cont} -{status} {tarefa["descricao"]}")
             cont=cont+1
     elif opcao == "3":
+       
         print("Você escolheu Concluir Tarefa")
+
+        cont = 1
+
+        for tarefa in tarefas:
+
+            if tarefa["concluida"]:
+                status = "[x]"
+            else:
+                status = "[ ]"
+
+            print(f"{cont} - {status} {tarefa['descricao']}")
+
+            cont = cont + 1
+
+        numero = int(input("Qual tarefa você quer concluir? "))
+
+        indice = numero - 1
+
+        tarefas[indice]["concluida"] = True
+
+
     elif opcao == "4":
         print("Você escolheu Excluir Tarefa")
     elif opcao == "0":
